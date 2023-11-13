bdd09280aa5c45eb599b67b8de99b157f7024a2b
JPratama7 <josepratama080@gmail.com>
2023-11-13 20:09:38 +0700

    feat(convert): add safestring and safebytes

--- convert/byte.go
+++ convert/byte.go
@@ -5,3 +5,10 @@ import "unsafe"
 func UnsafeBytes(s string) []byte {
 	return unsafe.Slice(unsafe.StringData(s), len(s))
 }
+
+func SafeBytes(s string) []byte {
+	b := UnsafeBytes(s)
+	tmp := make([]byte, len(b))
+	copy(tmp, b)
+	return tmp
+}
--- convert/string.go
+++ convert/string.go
@@ -5,3 +5,7 @@ import "unsafe"
 func UnsafeString(b []byte) string {
 	return unsafe.String(unsafe.SliceData(b), len(b))
 }
+
+func SafeString(b []byte) string {
+	return string(b)
+}
