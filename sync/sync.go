5e80e1e7d17ebcb435d6f8b361749fc9f44a015a
JPratama7 <josepratama080@gmail.com>
2023-12-16 18:51:41 +0700

    feat(sync): sync pool

---
+++ sync/sync.go
@@ -0,0 +1,27 @@
+package sync
+
+import "sync"
+
+type Pool[T any] struct {
+	m *sync.Pool
+}
+
+func NewPool[T any](f func() T) *Pool[T] {
+	return &Pool[T]{
+		m: &sync.Pool{
+			New: func() any { return f() },
+		},
+	}
+}
+
+func (p *Pool[T]) Get() T {
+	return p.m.Get().(T)
+}
+
+func (p *Pool[T]) Put(x T) {
+	p.m.Put(x)
+}
+
+func (p *Pool[T]) name() {
+	p.m.New()
+}
