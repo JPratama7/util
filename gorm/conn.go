4eb8404f408e6aa81e352f4ddaadb1ceca769836
JPratama7 <josepratama080@gmail.com>
2023-12-21 14:54:12 +0700

    feat: new gorm helper

---
+++ gorm/conn.go
@@ -0,0 +1,68 @@
+package gorm
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
+	grm "gorm.io/gorm"
+	"gorm.io/gorm/schema"
+	"time"
+)
+
+func NewMySQLConnection(connString string, cfg ...grm.Option) (db *grm.DB, err error) {
+	dial := mysql.Open(connString)
+
+	if len(cfg) == 0 {
+		cfg = append(cfg, &grm.Config{
+			NamingStrategy: schema.NamingStrategy{
+				SingularTable: true,
+			},
+		})
+	}
+
+	db, err = grm.Open(dial, cfg...)
+	if err != nil {
+		return
+	}
+
+	dbO, err := db.DB()
+	if err != nil {
+		panic(err)
+	}
+
+	dbO.SetConnMaxIdleTime(time.Duration(1) * time.Minute)
+	dbO.SetMaxIdleConns(2)
+	dbO.SetConnMaxLifetime(time.Duration(1) * time.Minute)
+
+	db.Statement.RaiseErrorOnNotFound = true
+
+	return
+}
+
+func NewPostgresConnection(connString string, cfg ...grm.Option) (db *grm.DB, err error) {
+	dial := postgres.Open(connString)
+
+	if len(cfg) == 0 {
+		cfg = append(cfg, &grm.Config{
+			NamingStrategy: schema.NamingStrategy{
+				SingularTable: true,
+			},
+		})
+	}
+
+	db, err = grm.Open(dial, cfg...)
+	if err != nil {
+		return
+	}
+
+	dbO, err := db.DB()
+	if err != nil {
+		panic(err)
+	}
+
+	dbO.SetConnMaxIdleTime(time.Duration(1) * time.Minute)
+	dbO.SetMaxIdleConns(2)
+	dbO.SetConnMaxLifetime(time.Duration(1) * time.Minute)
+	db.Statement.RaiseErrorOnNotFound = true
+
+	return
+}
