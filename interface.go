9ff5f30ad921d8f4df95d6600bde666a685d1e7a
JPratama7 <josepratama080@gmail.com>
2024-01-03 11:51:47 +0700

    feat: interface

---
+++ interface.go
@@ -0,0 +1,11 @@
+package fiber
+
+type Token[ID ~string, T IdGetter[ID]] interface {
+	Create(T) (ID, error)
+	Decode(ID) (T, error)
+	GetId(ID) (ID, error)
+}
+
+type IdGetter[ID ~string] interface {
+	GetId() ID
+}
