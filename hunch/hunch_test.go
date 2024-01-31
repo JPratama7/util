e9258f1415d8cb67dfd415b8d7a33c3e1b06128e
JPratama7 <josepratama080@gmail.com>
2024-01-31 14:14:17 +0700

    refactor: remove unused test
    Signed-off-by: JPratama7 <josepratama080@gmail.com>

--- hunch/hunch_test.go
+++ hunch/hunch_test.go
@@ -550,20 +550,6 @@ func TestThrow_ShouldReturnNilWhenAllExecutablesSucceed(t *testing.T) {
 	}
 }
 
-func TestMin(t *testing.T) {
-	t.Parallel()
-
-	if min(1, 2) != 1 {
-		t.Errorf("Should returns 1")
-	}
-	if min(-1, 2) != -1 {
-		t.Errorf("Should returns -1")
-	}
-	if min(54321, 12345) != 12345 {
-		t.Errorf("Should returns 12345")
-	}
-}
-
 func TestCok(t *testing.T) {
 	t.Parallel()
 
