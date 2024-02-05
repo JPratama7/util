c5b2edf0a16863192fc07f02cc207866320accd2
JPratama7 <josepratama080@gmail.com>
2024-02-05 12:47:29 +0700

    feat: use assert
    Signed-off-by: JPratama7 <josepratama080@gmail.com>

--- types/nil_test.go
+++ types/nil_test.go
@@ -1,31 +1,28 @@
 package types
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestNilStringReturnsNil(t *testing.T) {
 	result := NilString()
-	if result != nil {
-		t.Errorf("Expected nil, got %v", result)
-	}
+	assert.Nil(t, result, "Expected nil, got %v", result)
+
 }
 
 func TestNilBoolReturnsNil(t *testing.T) {
 	result := NilBool()
-	if result != nil {
-		t.Errorf("Expected nil, got %v", result)
-	}
+	assert.Nil(t, result, "Expected nil, got %v", result)
 }
 
 func TestNilIntReturnsNil(t *testing.T) {
 	result := NilInt()
-	if result != nil {
-		t.Errorf("Expected nil, got %v", result)
-	}
+	assert.Nil(t, result, "Expected nil, got %v", result)
+
 }
 
 func TestNilStructReturnsNil(t *testing.T) {
 	result := NilStruct()
-	if result != nil {
-		t.Errorf("Expected nil, got %v", result)
-	}
+	assert.Nil(t, result, "Expected nil, got %v", result)
 }
