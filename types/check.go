d01fd4f8d9576857d8ce306fff3001c34b4435c1
JPratama7 <josepratama080@gmail.com>
2024-02-05 14:14:31 +0700

    feat: add is iterable
    Signed-off-by: JPratama7 <josepratama080@gmail.com>

--- types/check.go
+++ types/check.go
@@ -9,3 +9,10 @@ func IsPointer(b any) bool {
 
 	return reflect.TypeOf(b).Kind() == reflect.Ptr
 }
+
+func IsIterable(b any) bool {
+	if b == nil {
+		return false
+	}
+	return reflect.TypeOf(b).Kind() == reflect.Slice || reflect.TypeOf(b).Kind() == reflect.Array || reflect.TypeOf(b).Kind() == reflect.Map
+}
