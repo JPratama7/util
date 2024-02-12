da1c0762cf7e3f3917f7377018de3d6acd569124
JPratama7 <josepratama080@gmail.com>
2024-02-12 08:57:33 +0700

    feat: new random string
    Signed-off-by: JPratama7 <josepratama080@gmail.com>

--- strings/cases.go
+++ strings/cases.go
@@ -1,6 +1,10 @@
 package strings
 
-import "github.com/JPratama7/util/convert"
+import (
+	"github.com/JPratama7/util/convert"
+	"math/rand"
+	"time"
+)
 
 func ToLower(b string) string {
 	res := make([]byte, len(b))
@@ -21,4 +25,24 @@ func ToUpper(b string) string {
 	}
 
 	return convert.UnsafeString(res)
+}
+
+// RandomString Based On Answer from https://stackoverflow.com/a/31832326
+func RandomString(n int) string {
+	src := rand.NewSource(time.Now().UnixNano())
+	b := make([]byte, n)
+	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return convert.UnsafeString(b)
 }
