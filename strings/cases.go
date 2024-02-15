0edb8fbf5fcd0d2519b0e7fcda0792aafd3a06b9
JPratama7 <josepratama080@gmail.com>
2024-02-15 16:14:33 +0700

    feat: customize elems
    Signed-off-by: JPratama7 <josepratama080@gmail.com>

--- strings/cases.go
+++ strings/cases.go
@@ -46,3 +46,21 @@ func RandomString(n int) string {
 
 	return convert.UnsafeString(b)
 }
+func RandomStringCustom(n int, elems string) string {
+	src := rand.NewSource(time.Now().UnixNano())
+	b := make([]byte, n)
+	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(elems) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return convert.UnsafeString(b)
+}
