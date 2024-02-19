dcbbe3f44c293db24c9267fded2c4c58caf7c91f
JPratama7 <josepratama080@gmail.com>
2024-02-19 15:12:12 +0700

    feat: new connection
    Signed-off-by: JPratama7 <josepratama080@gmail.com>

--- gorm/conn.go
+++ gorm/conn.go
@@ -38,6 +38,32 @@ func NewMySQLConnection(connString string, cfg ...grm.Option) (db *grm.DB, err e
 	return
 }
 
+func NewConnection(dial grm.Dialector, cfg ...grm.Option) (db *grm.DB, err error) {
+	if len(cfg) == 0 {
+		cfg = append(cfg, &grm.Config{
+			NamingStrategy: schema.NamingStrategy{
+				SingularTable: true,
+			},
+		})
+	}
+
+	db, err = grm.Open(dial, cfg...)
+	if err != nil {
+		return
+	}
+
+	dbO, err := db.DB()
+	if err != nil {
+		panic(err)
+	}
+
+	dbO.SetConnMaxIdleTime(time.Duration(1) * time.Minute)
+	dbO.SetMaxIdleConns(2)
+	dbO.SetConnMaxLifetime(time.Duration(1) * time.Minute)
+	db.Statement.RaiseErrorOnNotFound = true
+	return
+}
+
 func NewPostgresConnection(connString string, cfg ...grm.Option) (db *grm.DB, err error) {
 	dial := postgres.Open(connString)
 
