cb344a7cc1da8ddd2a24c5355fb91f7aa9115e9a
JPratama7 <josepratama080@gmail.com>
2024-07-17 17:31:51 +0700

    fix error because function parameter changed

--- token/paseto/default.go
+++ token/paseto/default.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Encode(id string, privateKey string) (string, error) {
-	return NewPASETO("", privateKey).Encode(id)
+	return NewPASETO("", privateKey).Encode("id", id)
 }
 
 func EncodeWithStruct[T any](id string, data *T, privateKey string) (string, error) {
@@ -20,17 +20,17 @@ func EncodeWithStructDuration[T any](id string, data *T, privateKey string, dur 
 }
 
 func EncodeforHours(id string, privateKey string, hours int32) (string, error) {
-	return NewPASETO("", privateKey, time.Duration(hours)*time.Hour).Encode(id)
+	return NewPASETO("", privateKey, time.Duration(hours)*time.Hour).Encode("id", id)
 
 }
 
 func EncodeforMinutes(id string, privateKey string, minutes int32) (string, error) {
-	return NewPASETO("", privateKey, time.Duration(minutes)*time.Hour).Encode(id)
+	return NewPASETO("", privateKey, time.Duration(minutes)*time.Hour).Encode("id", id)
 
 }
 
 func EncodeforSeconds(id string, privateKey string, seconds int32) (string, error) {
-	return NewPASETO("", privateKey, time.Duration(seconds)*time.Hour).Encode(id)
+	return NewPASETO("", privateKey, time.Duration(seconds)*time.Hour).Encode("id", id)
 
 }
 
